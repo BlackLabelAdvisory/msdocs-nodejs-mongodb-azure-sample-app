e824c489f3cd2cd6b4f5653ad3e37ccf66adf8b8
BlackLabelAdvisory <jack@blacklabeladvisoryptyltd.onmicrosoft.com>
2024-12-18 21:54:42 +0000

    Updates to Identity manager

--- controllers/graphController.js
+++ controllers/graphController.js
@@ -4,20 +4,27 @@
 const appServiceName = process.env.WEBSITE_SITE_NAME;
 
 const graphHelper = require('../utils/graphHelper');
+const { DefaultAzureCredential } = require("@azure/identity");
 
 exports.getProfilePage = async(req, res, next) => {
 
+    const defaultAzureCredential = new DefaultAzureCredential();
+
     try {
+
+        // get app's access token scoped to Microsoft Graph
+        const tokenResponse = await defaultAzureCredential.getToken("https://graph.microsoft.com/.default");
+
         // get user's access token scoped to Microsoft Graph from session
         // use token to create Graph client
         const graphClient = graphHelper.getAuthenticatedClient(req.session.protectedResources["graphAPI"].accessToken);
 
-        // return user's profile
-        const profile = await graphClient
-            .api('/me')
+        // return profiles of users in Graph
+        const users = await graphClient
+            .api('/users')
             .get();
 
-        res.render('profile', { isAuthenticated: req.session.isAuthenticated, profile: profile, appServiceName: appServiceName });   
+        res.render('users', { user: req.session.user, users: users });  
     } catch (error) {
         next(error);
     }
